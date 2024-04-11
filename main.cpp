8a549b7308b6d18f6ec8a285ded419da7be1ea29
Ilya Berulava <ilyaberulava@icloud.com>
2024-04-11 12:58:59 +0300

    добавил класс цели и изменил систему уравнения, на данный момент осталось написать систему для подстеча поперечной перегрузки и вывод графиков

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 // Параша по расчету внешней баллистики Атаки. Она двухрежимная, бог знает какая схема, утка вроде
-// задачу стараюсь выполнить в ООП, чтобы было красиво, но всякого полиморфизма тут не ждите, в падлу думать.
-//Класс ракеты в одноименном файле, в нем также некая инструкиция по использованию и обозначения
+// задачу стараюсь выполнить в ООП, чтобы было красиво.
+//Класс ракеты в одноименном файле, в нем также некая инструкиция по использованию и обозначения, аналогично и с классом цели.
 #include <iostream>
 #include "rocket/rocket.h"
 #include "system/system.cpp"
+#include "target/target.h"
+
 int main(){
     Rocket ATAKA;
     std::cout <<"отн запас топлива для 1 режима: "<< ATAKA.get_mu_1()<< std::endl;
@@ -18,6 +20,31 @@ int main(){
     std::cout <<"потребная тяга на 2 режиме "<< ATAKA.get_req_traction_2()<< std::endl;
     std::cout <<"суммарный потребный импульс "<< ATAKA.I_summ_req()<< std::endl;
 //     вызов функции, решающая систему уравнений и выводящая графики
-    system(ATAKA);
+
+    std::vector<double> dV_dT;
+    dV_dT.push_back(ATAKA.get_initial_speed());
+    std::vector<double> dtetta_dT;
+    std::vector<double> dx_dT;
+    dx_dT.push_back(0);
+    std::vector<double> dy_dT;
+    dy_dT.push_back(0);
+    std::vector<double> dm_dT;
+    dm_dT.push_back(ATAKA.get_m_0());
+
+    int i = 0;
+
+    system(ATAKA, dV_dT, dtetta_dT,dx_dT,dy_dT, dm_dT, i);
+    std::cout<<  "\nскорость: \n";
+    for (double k : dV_dT) {
+        std::cout<< k << " ";
+    }
+    std::cout<<  "\nугол: \n";
+    for (double k : dtetta_dT) {
+        std::cout<< k << " ";
+    }
+    std::cout<<  "\nмасса: \n";
+    for (double k : dm_dT) {
+        std::cout<< k << " ";
+    }
     return 0;
 }
--- system/system.cpp
+++ system/system.cpp
@@ -4,40 +4,56 @@
 #include <iostream>
 #include <numeric>
 #include "../rocket/rocket.h"
+void system_one(Rocket &ATAKA,std::vector<double>& dV_dT, std::vector<double>& dtetta_dT, std::vector<double>& dx_dT, std::vector<double>& dy_dT,std::vector<double>& dm_dT, int i);
+void system_two(Rocket &ATAKA,std::vector<double> &dV_dT, std::vector<double>& dtetta_dT, std::vector<double>& dx_dT, std::vector<double>& dy_dT,std::vector<double>& dm_dT,int i);
+double n_y(double V,double tetta,double x,double y,double i);
 
-void system_one(Rocket &ATAKA,std::vector<double> dV_dT, std::vector<double> dtetta_dT, std::vector<double> dx_dT, std::vector<double> dy_dT,std::vector<double> dm_dT, double i);
-void system_two(Rocket &ATAKA,std::vector<double> dV_dT, std::vector<double> dtetta_dT, std::vector<double> dx_dT, std::vector<double> dy_dT,std::vector<double> dm_dT,double i);
-void system(Rocket &ATAKA ){
-    std::vector<double> dV_dT;
-    dV_dT.push_back(ATAKA.get_initial_speed());
-    std::vector<double> dtetta_dT;
-    dV_dT.push_back(ATAKA.get_initial_speed()); // какая тетта изначальная?
-    std::vector<double> dx_dT;
-    dV_dT.push_back(0);
-    std::vector<double> dy_dT;
-    dV_dT.push_back(0);
-    std::vector<double> dm_dT;
-    dV_dT.push_back(ATAKA.get_m_0());
-
-    double i = 0;
-    while (i <=ATAKA.get_mode_1_work_time() ){
+// полная система, объединяющая 2 режима
+void system(Rocket &ATAKA ,std::vector<double>& dV_dT, std::vector<double>& dtetta_dT, std::vector<double>& dx_dT, std::vector<double>& dy_dT,std::vector<double>& dm_dT, int i){
+    int steps_in_sec = 100;
+    while (i <=ATAKA.get_mode_1_work_time() * steps_in_sec ){
         system_one(ATAKA,dV_dT,dtetta_dT,dx_dT,dy_dT,dm_dT,i);
         i++;
     }
-    while (i <=ATAKA.get_mode_2_work_time() ){
+    while (i <=ATAKA.get_mode_2_work_time()* steps_in_sec ){
         system_two(ATAKA,dV_dT,dtetta_dT,dx_dT,dy_dT,dm_dT,i);
         i++;
     }
+
+}
+//тут система для первого режима
+void system_one(Rocket &ATAKA,std::vector<double>& dV_dT, std::vector<double>& dtetta_dT, std::vector<double>& dx_dT, std::vector<double>& dy_dT,std::vector<double>& dm_dT, int i){
+    double C_x = 0.6;
+    double rho = 1.2 * 100000;
+    double S_m = 3.14 * (ATAKA.get_d()*ATAKA.get_d())/4;
+    double S_kr = 0.07;
+    double V = std::accumulate(dV_dT.begin(), dV_dT.end(), 0);// скорость (сумма изменения )
+    double tetta = std::accumulate(dtetta_dT.begin(), dtetta_dT.end(), 0);// угол (сумма изменения )
+    double x = std::accumulate(dx_dT.begin(),dx_dT.end(), 0);// х (сумма изменения )
+    double y = std::accumulate(dy_dT.begin(),dy_dT.end(), 0);// у (сумма изменения )
+
+    dV_dT.push_back( ATAKA.get_req_traction_1()- C_x*S_m*(rho * V*V/2) ); //изменение скорости(пушится в массив)
+    dtetta_dT.push_back((9.81/V)*(n_y(V,tetta,x,y,i) - cos(tetta)));//изменение тетта (пушится в массив)
+    dx_dT.push_back(V*cos(tetta));
+    dy_dT.push_back(V*sin(tetta));
+    dm_dT.push_back(-(ATAKA.get_req_traction_1()/ATAKA.get_I_ud_1_mode()));
+
+};
+
+//тут система для второго режима
+void system_two(Rocket &ATAKA,std::vector<double> &dV_dT, std::vector<double>& dtetta_dT, std::vector<double>& dx_dT, std::vector<double>& dy_dT,std::vector<double>& dm_dT,int i){
+
+};
+
+//функция радиуса
+double r(double x,double y,int i){
+
 }
 
-void system_one(Rocket &ATAKA,std::vector<double> dV_dT, std::vector<double> dtetta_dT, std::vector<double> dx_dT, std::vector<double> dy_dT,std::vector<double> dm_dT,double i){
-    double C_x = 0.6;
-    dV_dT[i] = ATAKA.get_req_traction_1() - C_x ; // тут с Cx какая- то залупивка + нужно добавить площадь ракеты плюс распределение плотности
-    double V = std::accumulate(dV_dT.begin(), dV_dT.end(), 0);
-
-    dtetta_dT[i] = (9.81/ V); // как лучше перегрузку взять?
-};
-void system_two(Rocket &ATAKA,std::vector<double> dV_dT, std::vector<double> dtetta_dT, std::vector<double> dx_dT, std::vector<double> dy_dT,std::vector<double> dm_dT,double i ){
+// функция перегрузки
+double n_y(double V,double tetta,double x,double y,double i){
+//    double r_t = ()/(r(x,y,i));// тут можно усложнить
+    return
 
 };
 
